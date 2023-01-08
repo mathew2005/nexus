ca7f78016e97b8bebcc707a8811dbd3637be9af0
Mathewos Wondwosen <89460058+mathew2005@users.noreply.github.com>
2023-01-08 18:42:09 +0300

    a lot of changes

--- studybud/settings.py
+++ studybud/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v2yg*(*m)k_3z$ppyc74n=-y*zl3163dw+faym-o=zu$i=s117'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.netlify.com']
 
 
 # Application definition
